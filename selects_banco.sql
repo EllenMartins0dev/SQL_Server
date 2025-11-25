5b7cf9e58551e9b53acbfc83067bb19b164da48c
Ellen Fernanda Martins <140816704+EllenMartins0dev@users.noreply.github.com>
2025-11-25 16:03:30 -0300

    Update SQL script to use 'Biblioteca' schema
    Alterando os selects para colocar  nome do banco antes da chamada da tabela, para ser possível fazer o select mesmo sem estar no banco referenciado.

--- selects_banco.sql
+++ selects_banco.sql
@@ -1,34 +1,38 @@
 -- Script SQL com duas a cinco consultas usando SELECT (com WHERE, ORDER BY, LIMIT, JOIN etc.);
 
--- 1. Seleciona cliente com código 2
-SELECT * FROM dbo.Clientes
+-- 1. Seleciona cliente com cÃ³digo 2
+SELECT * FROM Biblioteca.dbo.Clientes
 WHERE codigo_cliente = 2;
 
--- 2. Seleciona livros com preço entre 39.90 e 49.90
-SELECT * FROM dbo.Livros
+-- 2. Seleciona livros com preÃ§o entre 39.90 e 49.90
+SELECT * FROM Biblioteca.dbo.Livros
 WHERE preco BETWEEN 39.90 AND 49.90;
-'
+
 -- 3. Lista todos os autores ordenados por data de nascimento
-SELECT * FROM dbo.Autor
+SELECT * FROM Biblioteca.dbo.Autor
 ORDER BY data_nascimento;
 
--- 4. Seleciona os 3 primeiros gêneros
+-- 4. Seleciona os 3 primeiros gÃªneros
 SELECT TOP 3 * 
-FROM dbo.Genero;
+FROM Biblioteca.dbo.Genero;
 
--- 5. Seleciona clientes e seus endereços no bairro "Centro"
-SELECT cli.codigo_cliente, cli.nome, clie.bairro  FROM dbo.Clientes_Enderecos clie
-INNER JOIN dbo.Clientes cli 
+-- 5. Seleciona clientes e seus endereÃ§os no bairro "Centro"
+SELECT cli.codigo_cliente, cli.nome, clie.bairro  FROM Biblioteca.dbo.Clientes_Enderecos clie
+INNER JOIN Biblioteca.dbo.Clientes cli 
     ON cli.codigo_cliente = clie.codigo_cliente
 WHERE clie.bairro = 'Centro';
 
--- 6. Consulta estoque, livros e movimentações para o livro de código 1
+-- 6. Consulta estoque, livros e movimentaÃ§Ãµes para o livro de cÃ³digo 1
 SELECT est.codigo_estoque, mov.tipo_movimentacao, est.quantidade, est.codigo_livro, liv.titulo, liv.ISBN
-FROM dbo.Estoque est
-INNER JOIN dbo.Livros liv 
+FROM Biblioteca.dbo.Estoque est
+INNER JOIN Biblioteca.dbo.Livros liv 
     ON liv.codigo_livro = est.codigo_livro
-INNER JOIN dbo.Movimentacao_Estoque mov 
+INNER JOIN Biblioteca.dbo.Movimentacao_Estoque mov 
     ON mov.codigo_estoque = est.codigo_estoque
 WHERE est.codigo_livro = 1;
+
+
+
+
 
 
